4e1f3153a45a2273a0fff12cb5cb1843d41bbd99
Randy Heydon <randy.heydon@clockworklab.net>
2013-08-12 20:57:14 -0400

    More subspace tests for checking if subspaces are in other subspaces.
    Of course they still fail, and may need later additions.

--- sympy/ndgeometry/tests/test_subspace.py
+++ sympy/ndgeometry/tests/test_subspace.py
@@ -1,7 +1,11 @@
-from sympy import symbols, sin, cos, pi
+from sympy import symbols, sin, cos, pi, Equality, Not
+
 from sympy.ndgeometry.subspace import Subspace
+from sympy.ndgeometry.global_space import global_space as gl
+
 
 a, b, c, r, t, x, y, z = symbols('a b c r t x y z')
+
 
 def test_parameters():
     # Symbols that are not parameters.
@@ -26,3 +30,27 @@ def test_parents():
     assert point.in_ancestor(1) == Subspace([1, pi/2, 3], [], cylinder)
     assert point.in_ancestor(2) == Subspace([0, 1, 3], [])
     assert point.in_ancestor() == point.in_ancestor(2)
+
+
+def test_contains():
+    # When in parent/grandparent space.
+    cylinder = Subspace([r*cos(t), r*sin(t), z], [r, t, z])
+    circle = Subspace([1, a*2*pi, 3], [a], cylinder)
+    point1 = Subspace([0.25], [], circle)
+    point2 = Subspace([0, 0, 0], [])
+    assert point1 in point1
+    assert point1 in circle
+    assert circle in cylinder
+    assert point1 in cylinder
+    assert point2 not in circle
+    # When in other space.
+    circle2 = ([cos(t), sin(t), 3], [t])
+    point3 = Subspace([1, 0, 3], [])
+    assert point3 in circle
+    assert point2 in cylinder
+    assert circle2 in circle
+    # When including symbols.
+    point4 = Subspace([0, 1, b], [])
+    assert (point4 in circle) == Equality(b, 3)
+    assert (point4 not in circle) == Not(Equality(b, 3))
+    assert point4 in cylinder
